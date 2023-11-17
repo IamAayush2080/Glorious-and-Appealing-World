8d763f5f6183d666e213089e3be3760d53eba5b5
Aayush Kumar Kurmi <148619737+IamAayush2080@users.noreply.github.com>
2023-11-17 14:04:12 +0545

    Update index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>NATURE✔</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+    }
+
+    .container {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 20px;
+    }
+
+    h1 {
+      font-size: 36px;
+      color: #333333;
+      text-align: center;
+      margin-bottom: 20px;
+    }
+
+    p {
+      font-size: 18px;
+      color: #666666;
+      line-height: 1.5;
+      margin-bottom: 10px;
+    }
+
+    img {
+      max-width: 100%;
+      height: auto;
+      margin-bottom: 20px;
+    }
+
+    .button {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #3498db;
+      color: #ffffff;
+      text-decoration: none;
+      border-radius: 4px;
+      transition: background-color 0.3s ease;
+    }
+
+    .button:hover {
+      background-color: #2980b9;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Welcome to the Glorious and Appealing World!</h1>
+    <p>Discover the breathtaking landscapes, fascinating wildlife, and environmental wonders of our planet.</p>
+    <img src="path/to/image.jpg" alt="Beautiful scenery">
+    <a href="#" class="button">Explore More</a>
+  </div>
+</body>
+</html>
