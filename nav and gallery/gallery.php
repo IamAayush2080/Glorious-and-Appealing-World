393ed41f924ef808ed40ceca718bae961330a83d
Aayush Kumar Kurmi <148619737+IamAayush2080@users.noreply.github.com>
2023-11-19 06:04:20 +0000

    add

---
+++ nav and gallery/gallery.php
@@ -0,0 +1,110 @@
+<section class='my-5'>
+      <div class='py-5'>
+               <h2 class='text-center'>Our Gallery</h2>
+                   </div>
+                   <div class="container-fluid">
+                        <div class="row"> 
+                               <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/12.jpg" class="img-fluid pb-3">
+                                </div> 
+                                 <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/12.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/12.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/11.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/11.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/11.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/10.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/10.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/10.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/14.jpg" class="img-fluid pb-3">
+                                </div> 
+                                 <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/14.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/14.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/15.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/15.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/15.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/13.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/13.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/13.jpg" class="img-fluid pb-3">
+                                  </div>  
+                                      <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/31.jpg" class="img-fluid pb-3">
+                                </div> 
+                                 <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/31.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/31.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/32.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/32.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/32.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/33.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/33.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/33.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/34.jpg" class="img-fluid pb-3">
+                                </div> 
+                                 <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/34.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/34.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/35.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/35.jpg" class="img-fluid pb-3">
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-12">  
+                                     <img src="image/35.jpg" class="img-fluid pb-3">
+                               
+                                </div>
+                        </div>
+                 </div>   
+         </div>
+</section>
